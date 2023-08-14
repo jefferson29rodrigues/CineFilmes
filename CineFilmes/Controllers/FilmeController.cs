0addc878cb4f1ac85f1b5370d9d136691ce65b23
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2023-08-14 14:51:24 -0300

    cria rota inicial adiciona filme

---
+++ CineFilmes/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using CineFilmes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineFilmes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Genero);
+            Console.WriteLine(filme.Duracao);
+        }
+    }
+}
---
+++ CineFilmes/Models/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace CineFilmes.Models
+{
+    public class Filme
+    {
+        //public string Id { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+    }
+}
