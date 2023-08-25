213bf46046f6ea926651b44a35a351a6f17970f7
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2023-08-24 23:41:40 -0300

    Cria rota get todos filmes e rota filme por id

--- CineFilmes/Controllers/FilmeController.cs
+++ CineFilmes/Controllers/FilmeController.cs
@@ -8,14 +8,28 @@ namespace CineFilmes.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Genero);
             Console.WriteLine(filme.Duracao);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme? RecuperaFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
